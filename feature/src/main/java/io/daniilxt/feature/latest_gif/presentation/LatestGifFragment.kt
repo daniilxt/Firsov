e6fa1d1dd2ab2aea084b18ce1eabe37431cd1880
Daniil Firsov <daniil-firsov@mail.ru>
2021-09-04 23:31:44 +0300

    Gradle imports refactoring

--- feature/src/main/java/io/daniilxt/feature/latest_gif/presentation/LatestGifFragment.kt
+++ feature/src/main/java/io/daniilxt/feature/latest_gif/presentation/LatestGifFragment.kt
@@ -1,10 +1,23 @@
 package io.daniilxt.feature.latest_gif.presentation
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import coil.Coil
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.load
+import com.bumptech.glide.GenericTransitionOptions
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import io.daniilxt.common.di.FeatureUtils
 import io.daniilxt.common.extensions.clearLightStatusBar
 import io.daniilxt.common.extensions.setStatusBarColor
@@ -12,6 +25,7 @@ import io.daniilxt.feature.R
 import io.daniilxt.feature.databinding.FragmentLatestGifBinding
 import io.daniilxt.feature.di.FeatureApi
 import io.daniilxt.feature.di.FeatureComponent
+import timber.log.Timber
 import javax.inject.Inject
 
 class LatestGifFragment : Fragment() {
@@ -41,9 +55,61 @@ class LatestGifFragment : Fragment() {
         requireActivity().setStatusBarColor(R.color.white)
         requireView().clearLightStatusBar()
 
-        // setImage("http://static.devli.ru/public/images/gifs/202105/338eec95-f956-4aa6-8844-219166979cc2.gif")
+        setImage("http://static.devli.ru/public/images/gifs/202105/338eec95-f956-4aa6-8844-219166979cc2.gif")
     }
 
+
+    private fun setImage2(path: String) {
+
+        val circularProgressDrawable = CircularProgressDrawable(requireContext())
+        circularProgressDrawable.strokeWidth = 8f
+        circularProgressDrawable.centerRadius = 40f
+        circularProgressDrawable.setColorSchemeColors(Color.BLUE)
+        circularProgressDrawable.start()
+
+        Glide.with(this)
+            .load(path)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Timber.i("GLIDE ERROR ${e.toString()}")
+                    circularProgressDrawable.stop()
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    dataSource: com.bumptech.glide.load.DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+            })
+            .transition(GenericTransitionOptions.with(android.R.anim.fade_in))
+            .placeholder(circularProgressDrawable)
+            .into(binding.frgLatestGifGifViewer.includeGifViewerIvImage)
+    }
+
+    private fun setImage(path: String) {
+        val imageLoader = ImageLoader.Builder(requireContext())
+            .componentRegistry {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    add(ImageDecoderDecoder(requireContext()))
+                } else {
+                    add(GifDecoder())
+                }
+            }
+            .build()
+        Coil.setImageLoader(imageLoader)
+        binding.frgLatestGifGifViewer.includeGifViewerIvImage.load(path) {
+        }
+    }
 
     private fun inject() {
         FeatureUtils.getFeature<FeatureComponent>(this, FeatureApi::class.java)
